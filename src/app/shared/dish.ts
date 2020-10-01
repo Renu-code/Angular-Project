167596f6c0164323b67ed260f808cce544a2497b
Renu Gangele <renugangele08.rr@gmail.com>
2020-10-01 18:59:23 +0530

    Angular componenet part1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish{
+    id:string;
+    name:string;
+    image:string;
+    label:string;
+    featured:boolean;
+    category:string;
+    price:string;
+    description:string;
+}
